f9835f62ffd6b37f68bcb20dfacfb459281a4a77
eduar766 <eduar766@gmail.com>
2025-03-11 20:29:13 -0300

    Añade métodos para buscar y guardar claves API por correo electrónico en FirebaseApiKeyRepository

--- src/infrastructure/adapters/firebase-api-key.repository.ts
+++ src/infrastructure/adapters/firebase-api-key.repository.ts
@@ -9,4 +9,14 @@ export class FirebaseApiKeyRepository {
     const snapshot = await this.collection.where('key', '==', apiKey).get();
     return !snapshot.empty;
   }
+
+  async findByEmail(email: string): Promise<any> {
+    const snapshot = await this.collection.where('email', '==', email).get();
+    if (snapshot.empty) return null;
+    return snapshot.docs[0].data(); // Retornar el primer documento encontrado
+  }
+
+  async save(email: string, apiKey: string): Promise<void> {
+    await this.collection.doc(email).set({ email: email, key: apiKey });
+  }
 }
