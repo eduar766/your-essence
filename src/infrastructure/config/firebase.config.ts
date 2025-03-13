fa960062f4c7cd31fc657790f30736b4bc24a1e3
eduar766 <eduar766@gmail.com>
2025-03-12 23:19:53 -0300

    Reemplaza la forma de manejar los saltos de línea en la clave privada de Firebase

--- src/infrastructure/config/firebase.config.ts
+++ src/infrastructure/config/firebase.config.ts
@@ -6,7 +6,9 @@ dotenv.config();
 const firebaseCredentials = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Reemplaza los saltos de línea en la clave
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.split(String.raw`\n`).join(
+    '\n',
+  ), // Reemplaza los saltos de línea en la clave
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
 };
